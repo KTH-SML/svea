4b60eaf6360fdd43a5299c3c7ba6ee243aee2d38
Frank Jiang <frankji@kth.se>
2021-03-10 22:26:39 +0100

    Adding compute_offset test

--- src/svea_core/scripts/platooning_examples/tests/test_c_ovrv.py
+++ src/svea_core/scripts/platooning_examples/tests/test_c_ovrv.py
@@ -21,3 +21,10 @@ def test_rotate2D_from_np():
     radians = math.radians(-90)
     rot_xy = rotate2D(xy, radians)
     np.testing.assert_almost_equal(rot_xy, np.array([0.0, 1.0]))
+
+def test_compute_offset():
+    spacing = 1.0 - BACKTOWHEEL - FRONTTOWHEEL
+    heading = math.radians(-45)
+    offset = compute_offset(spacing, heading)
+    np.testing.assert_almost_equal(offset,
+        np.array([math.sqrt(2.0)/2, math.sqrt(2.0)/2, 0, 0]))
