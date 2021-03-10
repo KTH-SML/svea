96377ffa28e8351391bf9f460f21ebdfc02a93f1
Frank Jiang <frankji@kth.se>
2021-03-10 23:52:04 +0100

    Adding C_OVRV model test

--- src/svea_core/scripts/platooning_examples/tests/test_c_ovrv.py
+++ src/svea_core/scripts/platooning_examples/tests/test_c_ovrv.py
@@ -1,3 +1,5 @@
+import pytest
+import time
 import rospy
 import math
 import numpy as np
@@ -8,6 +10,11 @@ from svea.simulators.sim_SVEA import SimSVEA
 from svea.models.bicycle import SimpleBicycleModel
 from svea.data import RVIZPathHandler
 from svea.models.cooperative import C_OVRV
+from svea_msgs.msg import lli_ctrl
+
+@pytest.fixture
+def node():
+    rospy.init_node("c_ovrv_test", anonymous=True)
 
 def test_rotate2D_from_tuple():
     xy = (1.0, 0.0)
@@ -54,8 +61,7 @@ def test_collect_platoon_pts():
     assert ys == [math.sqrt(2.0)/2 * 2, math.sqrt(2.0)/2 * 1]
     assert yaws == [math.radians(45), math.radians(45)]
 
-def test_not_reached_steady_state():
-    rospy.init_node('test')
+def test_not_reached_steady_state(node):
     steady_vel = 2.0
     leader = SVEAPlatoonMember(LocalizationInterface, [], [],
                                data_handler = RVIZPathHandler)
@@ -66,8 +72,7 @@ def test_not_reached_steady_state():
     at_steady_state = reached_steady_state(steady_vel, leader, [follower])
     assert not at_steady_state
 
-def test_reached_steady_state():
-    rospy.init_node('test')
+def test_reached_steady_state(node):
     steady_vel = 2.0
     leader = SVEAPlatoonMember(LocalizationInterface, [], [],
                                data_handler = RVIZPathHandler)
@@ -90,8 +95,7 @@ def test_compute_spacings():
     spacings = compute_spacings(leader, [follower])
     assert spacings == [0.5 - BACKTOWHEEL - FRONTTOWHEEL]
 
-def test_toggle_pause():
-    rospy.init_node('test')
+def test_toggle_pause(node):
     leader_sim = SimSVEA(SimpleBicycleModel())
     follower_sim = SimSVEA(SimpleBicycleModel())
     toggle_pause(leader_sim, [follower_sim])
@@ -101,3 +105,22 @@ def test_toggle_pause():
     toggle_pause(leader_sim, [follower_sim])
     assert not leader_sim.is_pause
     assert not follower_sim.is_pause
+
+def test_c_ovrv_model():
+    platoon_size = 1
+    num_neighbors = 1 # 0 for don't use communicated info
+    desired_time_headway = 0.3
+    k1 = 0.5
+    k2 = 0.5
+    k3 = 0.8
+    k4 = 0.8
+    k_gains = [k1, k2, k3, k4]
+    min_spacing = 0.3
+    init_velocity = 1.2  # initial target velocity
+    dt = 0.01
+
+    covrv = C_OVRV(platoon_size, num_neighbors, k_gains, min_spacing,
+                   desired_time_headway, init_velocity, dt=dt)
+    assert np.all(covrv.A == np.array([[0.0, -1.0], [0.5, -0.65]]))
+    assert np.all(covrv.b == np.array([0.0, -0.15]))
+    assert np.all(covrv.equilibrium_pt == np.array([1.86, 1.2]))
