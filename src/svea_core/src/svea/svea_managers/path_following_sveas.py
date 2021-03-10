2d0c4400686c5e6a904751bd8cb8c494435a833c
Frank Jiang <frankji@kth.se>
2021-03-10 18:51:43 +0100

    Adding platoon manager; fixed missing target velocity

--- src/svea_core/src/svea/svea_managers/path_following_sveas.py
+++ src/svea_core/src/svea/svea_managers/path_following_sveas.py
@@ -10,6 +10,7 @@ from svea_archetypes import SVEAManager
 # from svea.simulators.viz_utils import publish_path, publish_target
 from svea.data import TrajDataHandler
 from svea.controllers import pure_pursuit
+from svea.controllers.pure_pursuit import PurePursuitSpeedController
 
 ## PURE PURSUIT PARAMS ########################################################
 pure_pursuit.k = 0.6  # look forward gain
@@ -28,12 +29,12 @@ class SVEAPurePursuit(SVEAManager):
     vehicle that is performing path following using a pure-pursuit
     algorithm.
 
-    :param vehicle_name: Name of vehicle; used to initialize each
-                         software module.
-    :type vehicle_name: str
     :param localizer: A chosen localization interface class constructor
     :type localizer: class
-    :param controller: A chosen controller class constructor
+    :param controller: A chosen controller class constructor, this
+    should be a pure pursuit based controller. This has no default
+    controller so the specific pure pursuit controller has to be
+    specified
     :type controller: class
     :param traj_x: X coordinates of trajectory
     :type traj_x: list
@@ -42,13 +43,17 @@ class SVEAPurePursuit(SVEAManager):
     :param data_handler: A chosen data handler class constructor,
                          defaults to TrajDataHandler
     :type data_handler: class
+    :param vehicle_name: Name of vehicle; used to initialize each
+                         software module, defaults to ''
+    :type vehicle_name: str
     """
 
-    def __init__(self, vehicle_name, localizer, controller,
-                       traj_x, traj_y, data_handler=TrajDataHandler):
+    def __init__(self, localizer, controller, traj_x, traj_y,
+                 data_handler=TrajDataHandler, vehicle_name=''):
 
-        SVEAManager.__init__(self, vehicle_name, localizer, controller,
-                                   data_handler = data_handler)
+        SVEAManager.__init__(self, localizer, controller,
+                             data_handler = data_handler,
+                             vehicle_name = vehicle_name)
 
         self.update_traj(traj_x, traj_y)
 
@@ -92,11 +97,13 @@ class SVEAPurePursuit(SVEAManager):
                          + (curr_xy[1] - target_xy[1])**2)
 
         if dist > self.goto_thresh:
+            self.controller.target_velocity = self.goto_vel
             steering, velocity = \
                 self.controller.compute_control(self.state, target_xy)
             self.data_handler.update_target(self.controller.target)
             return steering, velocity
         else:
+            self.controller.target_velocity = 0.0
             steering = 0.0
             velocity = 0.0
             return steering, velocity
@@ -119,3 +126,46 @@ class SVEAPurePursuit(SVEAManager):
     def is_finished(self):
         """Check if pure-pursuit controller is finished or not"""
         return self.controller.is_finished
+
+
+class SVEAPlatoonMember(SVEAPurePursuit):
+    """Special case of SVEA pure pursuit object that is convenient for
+    controlling a platoon vehicle.
+
+    :param vehicle_name: Name of vehicle; used to initialize each
+                         software module.
+    :type vehicle_name: str
+    :param localizer: A chosen localization interface class constructor
+    :type localizer: class
+    :param controller: A chosen controller class constructor
+    :type controller: class
+    :param traj_x: X coordinates of trajectory
+    :type traj_x: list
+    :param traj_y: Y coordinates of trajectory
+    :type traj_y: list
+    :param data_handler: A chosen data handler class constructor,
+                         defaults to TrajDataHandler
+    :type data_handler: class
+    :param vehicle_name: Name of vehicle; used to initialize each
+                         software module, defaults to ''
+    :type vehicle_name: str
+    """
+
+    def __init__(self, localizer, traj_x, traj_y, data_handler=TrajDataHandler,
+                 vehicle_name=''):
+
+        SVEAPurePursuit.__init__(self, localizer,
+                                 PurePursuitSpeedController,
+                                 traj_x, traj_y,
+                                 data_handler = data_handler,
+                                 vehicle_name = vehicle_name)
+
+    def send_vel(self, vel):
+        self.controller.target_velocity = vel
+        steering, velocity = self.compute_control()
+        self.send_control(steering, velocity)
+
+    def send_accel(self, accel, dt):
+        self.controller.target_velocity += accel * dt
+        steering, velocity = self.compute_control()
+        self.send_control(steering, velocity)
