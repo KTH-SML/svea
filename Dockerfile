d8e2c660891d1bfdc38d41ba4289bbd183a83765
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2022-09-17 02:56:14 +0200

    add: noetic based docker files/tools

---
+++ Dockerfile
@@ -0,0 +1,66 @@
+# Dockerfile for SVEA image
+#
+# Author: Kaj Munhoz Arfvidsson
+
+ARG ROSDISTRO
+ARG WORKSPACE
+
+#####################
+## SVEA BASE IMAGE ##
+#####################
+
+FROM ros:$ROSDISTRO
+
+ARG ROSDISTRO
+ARG WORKSPACE
+ARG DEBIAN_FRONTEND=noninteractive
+
+## Install dependencies from apt-get
+
+RUN apt-get update -y && \
+    apt-get upgrade -y && \
+    apt-get install --no-install-recommends -y \
+        apt-utils \
+        git vim nano curl iputils-ping \
+        python3-pip \
+        python3-numpy \
+        python3-matplotlib \
+        python3-catkin-tools \
+        && \
+    rm -rf /var/lib/apt/lists/*
+
+## Create svea workspace
+
+COPY . $WORKSPACE
+WORKDIR $WORKSPACE
+
+RUN cp -f entrypoint /ros_entrypoint.sh && \
+    apt-get update -y && \
+    rosdep update \
+        --rosdistro $ROSDISTRO \
+        && \
+    rosdep install \
+        --rosdistro $ROSDISTRO \
+        --from-paths src \
+        --ignore-src \
+        -qry \
+        && \
+    rm -rf /var/lib/apt/lists/*
+
+# Run catkin build on workspace.
+# (improves build speeds later on)
+
+RUN catkin config \
+        --init \
+        --mkdirs \
+        --extend /opt/ros/$ROSDISTRO \
+        > /dev/null \
+        && \
+    catkin build || \
+    true
+
+# Container entrypoint (executes util/entrypoint)
+# bash is run by default when user starts
+
+ENTRYPOINT ["/ros_entrypoint.sh"]
+CMD ["bash"]
---
+++ util/config.sh
@@ -0,0 +1,95 @@
+#! /bin/sh
+#
+# This dot-script can be sourced for environment variables describing this
+# project.
+#
+# Author: Kaj Munhoz Arfvidsson
+
+
+################################################################################
+################################################################################
+
+
+##
+## https://gist.github.com/kaarmu/123f536abd46fc86b0d720c8137a13a7
+##
+
+
+# Print error and exit
+# > panic [CODE] MESSAGE
+panic() {
+    [ $# -gt 1 ] && CODE=$1 && shift || CODE=1
+    echo "Error ($CODE): $1"
+    usage
+    exit $CODE
+}
+
+# Assert there exist commands
+# > assert_command [COMMANDS...]
+assert_command() {
+    for cmd in "$@"; do
+        [ ! "$(command -v "$cmd")" ] && panic "Missing command \"$cmd\". Is it installed?"
+    done
+}
+
+# Return the argument at index
+# > index [-]NUM [ARGS...]
+index() {
+    [ $# -lt 2 ] && return 1
+    [ $1 -gt 0 ] && i=$1 || i=$(($# + $1))
+    [ $i -le 0 ] && echo "" || ( shift $i && echo "$1" )
+}
+
+# Replace first substring with something else
+# > replace SUB NEW TEXT
+replace() {
+    case "$3" in
+        *"$1"* ) echo "${3%%$1*}$2${3#*$1}" ;;
+        "$3" ) echo "$3" ;;
+    esac
+}
+
+# Climb directories to find an existing path CHILD
+# > climb CHILD [ PARENT ]
+climb() {
+    CHILD="$1"
+    PARENT="$(realpath "${2:-$PWD}")"
+    [ -e "$PARENT/$CHILD" ] && echo "$PARENT" && return 0
+    [ "$PARENT" = "/" ] && return 1
+    climb "$CHILD" "$PARENT/.."
+    return $?
+}
+
+
+################################################################################
+################################################################################
+
+
+ROSDISTRO="noetic"
+
+REPOSITORY_PATH="$(climb .git)"
+REPOSITORY_NAME="$(basename "$REPOSITORY_PATH")"
+
+BUILD_CONTEXT="$REPOSITORY_PATH"
+IMAGE_TAG="$(basename "$BUILD_CONTEXT")"
+IMAGE_TAG="${IMAGE_TAG%%.*}"
+CONTAINER_NAME="$REPOSITORY_NAME"
+
+WORKSPACE="/$REPOSITORY_NAME"
+SHRVOL_SRC="$REPOSITORY_PATH/src"
+SHRVOL_DST="$WORKSPACE/src"
+
+
+if [ -n "$DEBUG" ]; then
+    echo ROSDISTRO="$ROSDISTRO"
+    echo REPOSITORY_PATH="$REPOSITORY_PATH"
+    echo REPOSITORY_NAME="$REPOSITORY_NAME"
+    echo BUILD_CONTEXT="$BUILD_CONTEXT"
+    echo IMAGE_TAG="$IMAGE_TAG"
+    echo CONTAINER_NAME="$CONTAINER_NAME"
+    echo WORKSPACE="$WORKSPACE"
+    echo SHRVOL_SRC="$SHRVOL_SRC"
+    echo SHRVOL_DST="$SHRVOL_DST"
+    echo
+fi
+
