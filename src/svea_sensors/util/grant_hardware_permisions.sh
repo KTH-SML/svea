ab16c8e2ae7ff32dafb9e612f132e7a269f1670e
Mustafa Al-Janabi <ma970101@yahoo.se>
2023-05-25 09:57:57 +0200

    Update permission to include ttyACM* for RTK receiver permissions

--- src/svea_sensors/util/grant_hardware_permisions.sh
+++ src/svea_sensors/util/grant_hardware_permisions.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 echo 'KERNEL=="i2c-[18]*", GROUP="i2c"' >> /etc/udev/rules.d/10-local_i2c_group.rules
+echo 'KERNEL=="ttyACM[0-9]*", MODE="0666"' >> /etc/udev/rules.d/10-local_ttyACM.rules
 adduser $SUDO_USER dialout
 adduser $SUDO_USER i2c
-udevadm control --reload-rules && udevadm trigger --name=i2c-1 && udevadm trigger --name=i2c-8
+udevadm control --reload-rules && \ 
+    udevadm trigger --name=i2c-1 && \
+    udevadm trigger --name=i2c-8 && \
+    udevadm trigger --name=ttyACM0 && \
+    udevadm trigger --name=ttyACM1
 
