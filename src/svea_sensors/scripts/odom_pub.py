35357eb2e97fb255e86a143b70faa56aff3f509b
Fedezac <fedezac99@gmail.com>
2023-07-11 17:53:09 +0200

    Working, strnage drift between gps measurements

--- src/svea_sensors/scripts/odom_pub.py
+++ src/svea_sensors/scripts/odom_pub.py
@@ -2,18 +2,16 @@
 
 import rospy
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import TwistWithCovarianceStamped
 
 class Republish():
 
     def __init__(self):
         # Establish publisher of converted Twist message
-        self.odom_pub = rospy.Publisher(
-            '/odometry/gps',
-            Odometry,
-            queue_size=1,
-            latch=True)
+        self.odom_pub = rospy.Publisher('/odometry/gps', Odometry, queue_size=1, latch=True)
+        self.twist_pub = rospy.Publisher('/ekf_twist', TwistWithCovarianceStamped, queue_size=1, latch=True)
 
-    def pub(self):
+    def pub_odom_message(self):
         odom_corr = Odometry()
         cov_matrix = [0.1, 0.0, 0.0, 0.0, 0.0, 0.0,
                            0.0, 0.1, 0.0, 0.0, 0.0, 0.0,
@@ -22,17 +20,32 @@ class Republish():
                            0.0, 0.0, 0.0, 0.0, 0.1, 0.0,
                            0.0, 0.0, 0.0, 0.0, 0.0, 0.1]
         odom_corr.header.stamp = rospy.Time.now()
-        odom_corr.header.frame_id = 'odom'
+        odom_corr.header.frame_id = 'map'
         odom_corr.child_frame_id = 'base_link'
         odom_corr.pose.covariance = cov_matrix
         odom_corr.twist.covariance = cov_matrix
         self.odom_pub.publish(odom_corr)
+
+    def pub_twist_message(self):
+        twist = TwistWithCovarianceStamped()
+        cov_matrix = [0.1, 0.0, 0.0, 0.0, 0.0, 0.0,
+                           0.0, 0.1, 0.0, 0.0, 0.0, 0.0,
+                           0.0, 0.0, 0.1, 0.0, 0.0, 0.0,
+                           0.0, 0.0, 0.0, 0.1, 0.0, 0.0,
+                           0.0, 0.0, 0.0, 0.0, 0.1, 0.0,
+                           0.0, 0.0, 0.0, 0.0, 0.0, 0.1]
+        twist.header.stamp = rospy.Time.now()
+        twist.header.frame_id = 'base_link'
+        twist.twist.twist.linear.x = 0.0
+        twist.twist.covariance = cov_matrix
+        self.twist_pub.publish(twist)
 
 if __name__ == '__main__':
     rospy.init_node('odom_corrector', anonymous=False)
     ctt = Republish()
     rospy.loginfo("odom_corrector node successfuly initilized")
     while not rospy.is_shutdown():
-        ctt.pub()
-        rospy.sleep(1)
+        #ctt.pub_odom_message()
+        ctt.pub_twist_message()
+        rospy.sleep(.1)
     rospy.spin()
