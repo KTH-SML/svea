9815e809aafad0f6a6613eab3cb85df90d6988fc
Annika-wyt <yintingwong@gmail.com>
2023-08-02 16:59:55 +0200

    dev

--- src/svea_sensors/scripts/static_svea_pose.py
+++ src/svea_sensors/scripts/static_svea_pose.py
@@ -5,12 +5,18 @@ import numpy as np
 import rospy
 
 import tf
+import tf2_ros
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 from tf2_msgs.msg import TFMessage
 from sensor_msgs.msg import NavSatFix
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped, TransformStamped, Point, Quaternion
 from aruco_msgs.msg import Marker
+
+#TODO: add the transform from svea to aruco marker 
+#TODO: publish a statiic/pose for EKF global 
+
+#TODO: conditions for static gps and active gps
 
 class static_svea_gps:
 
@@ -18,10 +24,14 @@ class static_svea_gps:
         #initalize the node 
         rospy.init_node('static_svea_gps')
 
+        self.gps_fix_topic = rospy.get_param("~gps_fix", "static/gps/fix")
+        self.odometry_gps_topic = rospy.get_param("~odometry_gps", "static/odometry/gps")
+        self.aruco_pose_topic = rospy.get_param("~aruco_pose", "aruco_pose")
+
         #Subscriber
-        rospy.Subscriber("static/gps/fix", NavSatFix, self.gps_callback) # gps signal (lat long) of static svea
-        rospy.Subscriber("static/odometry/gps", TFMessage, self.odom_gps_callback) # position of static svea
-        rospy.Subscriber("aruco_pose", Marker, self.aruco_callback)
+        rospy.Subscriber(self.gps_fix_topic, NavSatFix, self.gps_callback) # gps signal (lat long) of static svea
+        rospy.Subscriber(self.odometry_gps_topic, TFMessage, self.odom_gps_callback) # position of static svea
+        rospy.Subscriber(self.aruco_pose_topic, Marker, self.aruco_callback)
         
         #Publisher
         self.pose_pub = rospy.Publisher("static/pose", PoseWithCovarianceStamped, queue_size=10) #publish to pose0 in ekf
@@ -29,10 +39,11 @@ class static_svea_gps:
         #Variable
         self.gps_msg = None
         self.location = None
-        self.aruco_pose = None
 
         #Transformation
-        self.listener = tf.TransformListener()
+        self.buffer = tf2_ros.Buffer(rospy.Duration(10))
+        self.listener = tf2_ros.TransformListener(self.buffer)
+        self.br = tf2_ros.TransformBroadcaster()
 
     def run(self):
         rospy.spin()
@@ -49,38 +60,24 @@ class static_svea_gps:
         self.publish_pose()
 
     def aruco_callback(self, msg):
-        #inverse ?
-        self.aruco_header = msg.header
-        self.aruco_pose = msg.pose
-        self.inverse_aruco()
+        try:
+            self.aruco_header = msg.header.stamp
+            transform = self.buffer.lookup_transform('camera', 'aruco11', self.aruco_header, rospy.Duration(0.5))
+            self.broadcast_aruco(transform.transform.translation, transform.transform.rotation, self.aruco_header)
+        except Exception as e: 
+            rospy.loginfo(f"Error, {e}")
 
     def publish_pose(self):
         self.pose_pub.publish(self.location)
 
-    def inverse_aruco(self):
-        try:
-            (trans, rot) = self.listener.lookupTransform('camera', 'aruco11', rospy.Time(0))
-            try:
-                transform_matrix = tf.transformations.concatenate_matrices(
-                    tf.transformations.translation_matrix(trans),
-                    tf.transformations.quaternion_matrix(rot))
-                try:
-                    inversed_transform = tf.transformations.inverse_matrix(transform_matrix)
-                    try:
-                        translation = tf.transformations.translation_from_matrix(inversed_transform)
-                        quaternion = tf.transformations.quaternion_from_matrix(inversed_transform)
-                        print("translation", translation)
-                        print("=================================================")
-                        print("quaternion", quaternion)
-                    except:
-                        rospy.loginfo("Cannot get translation/ quaternion from inversed matrix")                        
-                except:
-                    rospy.loginfo("Cannot inverse matrix")
-            except:
-                rospy.loginfo("Cannot concatenate matrices")
-        except:
-            rospy.loginfo("Cannot lookup transform")
-            
+    def broadcast_aruco(self, translation, quaternion, time):
+        msg = TransformStamped()
+        msg.header.stamp = time
+        msg.header.frame_id = "camera"
+        msg.child_frame_id = "arucofromtransform"
+        msg.transform.translation = translation
+        msg.transform.rotation = quaternion
+        self.br.sendTransform(msg)
 
 if __name__ == '__main__':
     static_svea_gps().run()
