fd17810cfef5a754c2c6269e30caa82c9c3178ef
Annika-wyt <yintingwong@gmail.com>
2023-08-04 13:48:37 +0200

    dev

--- src/svea_sensors/scripts/generate_waypoint.py
+++ src/svea_sensors/scripts/generate_waypoint.py
@@ -15,68 +15,18 @@ from std_msgs.msg import Float64MultiArray
 from visualization_msgs.msg import MarkerArray, Marker
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
-#import plotly.graph_objects as go
-
 class generate():
-    #lat, long
-    '''#    default_corners = [[59.35090600000875, 18.068049400004035], 
-    #                [59.35087019986695, 18.067948899970027] ,
-    #                [59.350880999981385, 18.067930400101968] ,
-    #                [59.35091747278197, 18.06802886626117]]
-
-    #    default_corners = [[59.35090538359178,18.068052862349326], 
-    #                [59.35088159984089, 18.06798270093596] ,
-    #                [59.350893398289955, 18.06796449531501] ,
-    #                [59.35091946020317, 18.068035701770267]]
-
-    #    default_corners = [[59.3508205 , 18.0679565],
-    #                       [59.350855, 18.0680513], 
-    #                       [59.350878, 18.0680259],
-    #                       [59.3508457, 18.0679246]] #carpark
-    #    default_corners = [[59.350703089697824, 18.067884361913592],
-    #                       [59.35066237301757, 18.067937795070453],
-    #                       [59.35067459897018, 18.067979029770026],
-    #                       [59.35072082906835, 18.06792115256776]] #padestrian
-
-    #    default_corners = [[59.350712, 18.0678711],
-    #                    [59.3506577, 18.0679434]] # straight line on padertrain
-
-    #    default_corners = [[59.3507694, 18.0678011],
-    #                    [59.3507132, 18.0678728]] # straight line on padertrain
-        
-    #    default_corners = [[59.3507715, 18.0677963],
-    #                    [59.3507224, 18.0678608]] # straight line on padertrain
-
-    #59.3507331
-    #longitude: 18.0678454
-
-
-    #59.3507141,18.067871
-    #59.3506717
-    #longitude: 18.0679268'''
-    
-#    default_corners = [[59.3508579, 18.0679108],
-#                       [59.3508927, 18.0680166],
-#                       [59.3508488, 18.0680664],
-#                        [59.3508068, 18.0679704]] #carpark2
-    
-    default_corners = [[59.3508586, 18.0679122],
+    corners = [[59.3508586, 18.0679122],
                        [59.3508938, 18.0680175],
                        [59.3508563, 18.0680577],
                         [59.3508059, 18.0679789]] #carpark3
 
-#    default_corners = [[59.3508059, 18.0679789],
-#                       [59.3508563, 18.0680577],
-#                       [59.3508938, 18.0680175],
-#                       [59.3508586, 18.0679122]] #carpark3 revrese
-
-#    default_corners = [[59.3508586, 18.0679122],
-#                       [59.3508938, 18.0680175]] #carpark3 first 2 points
-    
     def __init__(self):
+    
+        rospy.init_node("generate_waypoints", anonymous=False)
 
         self.waypoint_topic = rospy.get_param("~waypoints_topic", "/outdoor_localization_waypoint")
-        self.corners = rospy.get_param("~corners", self.default_corners)
+        self.default_corners = rospy.get_param("~corners", self.corners)
         self.resolution = rospy.get_param("~resolution", 10)
         self.location_topic = rospy.get_param("~location_topic", "/gps/filtered")
         self.vis_topic = rospy.get_param("~marker_topic", "/waypoints")
@@ -100,6 +50,9 @@ class generate():
 
         self.generate_waypoints()
 
+    def run(self):
+        rospy.spin()
+
     def gps_odometry_callback(self,msg):
         if self.coor is None:
             self.coor = [[msg.pose.pose.position.x, msg.pose.pose.position.y]]
@@ -110,48 +63,6 @@ class generate():
             #self.start = [59.3507979, 18.0679703]
             self.start = [msg.latitude, msg.longitude]
             print(self.start)
-
-    '''def show_on_map(self):
-        dc_array = np.array(self.default_corners)
-        color = [
-                    '#1f77b4',  # muted blue
-                    '#ff7f0e',  # safety orange
-                    '#2ca02c',  # cooked asparagus green
-                    '#d62728',  # brick red
-                    '#9467bd',  # muted purple
-                    '#8c564b',  # chestnut brown
-                    '#e377c2',  # raspberry yogurt pink
-                    '#7f7f7f',  # middle gray
-                    '#bcbd22',  # curry yellow-green
-                    '#17becf'   # blue-teal
-                ]
-        
-        for count, point in enumerate(dc_array):
-            if count == 0:
-                fig = go.Figure(go.Scattermapbox(
-                    name = "point" + str(count),
-                    fillcolor = color[count%len(color)],
-                    mode = "markers",
-                    lon = [point[1]],
-                    lat = [point[0]],
-                    marker = {'size': 10}))
-            else:
-                fig.add_trace(go.Scattermapbox(
-                    name = "point" + str(count),
-                    fillcolor = color[count%len(color)],
-                    mode = "markers",
-                    lon = [point[1]],
-                    lat = [point[0]],
-                    marker = {'size': 10}))
-
-        fig.update_layout(
-            margin ={'l':0,'t':0,'b':0,'r':0},
-            mapbox = {
-                'style': "open-street-map",
-                'center': {'lon': 18.0680513, 'lat': 59.350855},
-                'zoom': 18  })
-        
-        fig.show()'''
 
     def generate_waypoints(self):
         default_corners_r = np.radians(self.default_corners)
@@ -165,21 +76,8 @@ class generate():
             coor_y = distance * np.sin(brng) + self.coor[0][1]
 
             self.coor.append([coor_x, coor_y])
-            #self.coor.append(temp[count])
             print(brng, distance, self.coor[-1])
     
-        #working
-        '''for count, point in enumerate(default_corners_r):
-            y = np.sin(point[1]-default_corners_r[0][1])*np.cos(point[0])
-            x = np.cos(default_corners_r[0][0])*np.sin(point[0])-np.sin(default_corners_r[0][0])*np.cos(point[0])*np.cos(point[1]-default_corners_r[0][1])
-            brng = np.pi/2 - np.arctan2(y, x)
-            distance = geodistnace.geodesic(np.degrees(point), np.degrees(default_corners_r[0]), ellipsoid='GRS-80').m
-            coor_x = distance * np.cos(brng)
-            coor_y = distance * np.sin(brng)
-
-            self.coor.append([coor_x, coor_y])
-            print(brng, distance, self.coor[-1])'''
-        
         pub_points = Float64MultiArray()
         pub_points.data = list(chain.from_iterable(self.coor[1:]))
         self.visualize()
@@ -215,15 +113,6 @@ class generate():
             green += 0.2
 
         self.pub_visualization.publish(marker_array)
-#    coor_array = np.array(coor)
-#    color_list = ['b','g','r','c','m','y','k','w']
-#    for count1, coor_ in enumerate(coor_array):
-#        plt.plot(coor_[0], coor_[1], 'o', color=color_list[count1])
-#    plt.show()
 
 if __name__ == '__main__':
-    rospy.init_node("generate_waypoints", anonymous=False)
-    rate = rospy.Rate(10)
-    generat_points = generate()
-    while not rospy.is_shutdown():
-        rate.sleep()
+    generate().run()
--- src/svea_sensors/scripts/static_svea_pose.py
+++ src/svea_sensors/scripts/static_svea_pose.py
@@ -25,9 +25,11 @@ class static_svea_gps:
         #initalize the node 
         rospy.init_node('static_svea_gps')
 
-        self.gps_fix_topic = rospy.get_param("~gps_fix", "static/gps/fix")
-        self.odometry_gps_topic = rospy.get_param("~odometry_gps", "static/odometry/gps")
-        self.aruco_pose_topic = rospy.get_param("~aruco_pose", "aruco_pose")
+        self.gps_fix_topic = rospy.get_param("~static_gps_fix_topic", "static/gps/fix")
+        self.odometry_gps_topic = rospy.get_param("~odometry_gps_topic", "static/odometry/gps")
+        self.aruco_pose_topic = rospy.get_param("~aruco_pose_topic", "aruco_pose")
+
+        self.aruco_id = rospy.get_param("~aruco_id", 11)
 
         #Subscriber
         rospy.Subscriber(self.gps_fix_topic, NavSatFix, self.gps_callback) # gps signal (lat long) of static svea
@@ -41,9 +43,8 @@ class static_svea_gps:
         self.gps_msg = None
         self.location = None
 
-        self.origin_x_aruco = 0
-        self.origin_y_aruco = 0
-        
+        self.frame = 'aruco' + str(self.aruco_id)
+
         #Transformation
         self.buffer = tf2_ros.Buffer(rospy.Duration(10))
         self.listener = tf2_ros.TransformListener(self.buffer)
@@ -63,58 +64,20 @@ class static_svea_gps:
             self.location.pose = msg.pose
 
     def aruco_callback(self, msg):
-        if msg.id == 11:
-#            try:
-                #CORRECT ARUCO11 TO MAP
-                self.aruco_header = msg.header.stamp
-                self.aruco_pose = PoseStamped()
-                self.aruco_pose.header.stamp = rospy.Time.now() #from map to aruco11
-                self.aruco_pose.header.frame_id = "map"
-                self.aruco_pose.pose.position.x = 0.0
-                self.aruco_pose.pose.position.y = 0.0
-                self.aruco_pose.pose.orientation.w = 1.0
+        if msg.id == self.aruco_id:
+            try:
+                transform = self.buffer.lookup_transform('base_link', self.frame, self.aruco_header, rospy.Duration(0.5))
 
-#                transform = self.buffer.lookup_transform('aruco11', 'camera', self.aruco_header, rospy.Duration(0.5))
-                
-                #MEASURED ARUCO11 TO MAP
-                transform = self.buffer.lookup_transform('map', 'aruco11', self.aruco_header, rospy.Duration(0.5))
-                rospy.loginfo(f"transform{transform}")
-                
-                #ERROR BETWEEN CORRECT AND MEASURED
-                error = Point()
-                error.x = self.aruco_pose.pose.position.x - transform.transform.translation.x
-                error.y = self.aruco_pose.pose.position.y - transform.transform.translation.y
-                error.z = self.aruco_pose.pose.position.z - transform.transform.translation.z
+                self.broadcast_aruco(transform.transform.translation, transform.transform.rotation, self.aruco_header)
+                self.publish_pose(transform.transform.translation, transform.transform.rotation, self.aruco_header)
 
-                transform2 = self.buffer.lookup_transform('map', 'base_link', self.aruco_header, rospy.Duration(0.5))
-                final_transform = self.buffer.lookup_transform('base_link', 'camera', self.aruco_header, rospy.Duration(0.5))
-#                final_transform.transform.translation.x = transform2.transform.translation.x - error.x
-#                final_transform.transform.translation.y = transform2.transform.translation.y - error.y
-#                final_transform.transform.translation.z = transform2.transform.translation.z - error.z
-                rospy.loginfo(f"final_transform{final_transform}")
-                rospy.loginfo(f"error{error}")
-
-                self.broadcast_aruco(final_transform.transform.translation, final_transform.transform.rotation, self.aruco_header)
-                self.publish_pose(final_transform.transform.translation, final_transform.transform.rotation, self.aruco_header)
-
-#attempt2
-#                transform = self.buffer.lookup_transform('camera', 'aruco11', self.aruco_header, rospy.Duration(0.5))
-#                transform2 = self.buffer.lookup_transform('map', 'camera', self.aruco_header, rospy.Duration(0.5))
-#                rospy.loginfo(f"transform{transform2}")
-
-#attempt1
-#                transform = self.buffer.lookup_transform('camera', 'aruco11', self.aruco_header, rospy.Duration(0.5))
-#                do_trans = tf2_geometry_msgs.do_transform_pose(self.aruco_pose, transform)
-#                self.broadcast_aruco(do_trans.pose.position, do_trans.pose.orientation, self.aruco_header)
-#                self.publish_pose(do_trans.pose.position, do_trans.pose.orientation, self.aruco_header)
-
-#            except Exception as e: 
-#                rospy.loginfo(f"Error, {e}")
+            except Exception as e: 
+                rospy.loginfo(f"Error, {e}")
 
     def publish_pose(self, translation, quaternion, time):
         msg = PoseWithCovarianceStamped()
         msg.header.stamp = time
-        msg.header.frame_id = "camera"
+        msg.header.frame_id = "map"
         msg.pose.pose.position = translation
         msg.pose.pose.orientation = quaternion
         self.pose_pub.publish(msg)
@@ -122,7 +85,7 @@ class static_svea_gps:
     def broadcast_aruco(self, translation, quaternion, time):
         msg = TransformStamped()
         msg.header.stamp = time
-        msg.header.frame_id = "camera"
+        msg.header.frame_id = "base_link"
         msg.child_frame_id = "arucofromtransform"
         msg.transform.translation = translation
         msg.transform.rotation = quaternion
