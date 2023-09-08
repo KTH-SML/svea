226c55fd029a3b8511e15324858580fc74984936
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2023-09-08 11:25:06 +0200

    fix: logging from rospy (#24)

--- src/svea_core/scripts/state_publisher.py
+++ src/svea_core/scripts/state_publisher.py
@@ -46,10 +46,10 @@ class state_publisher:
         self.sub_clicked_point = rospy.Subscriber(
             '/clicked_point',
             PointStamped,
-            lambda msg: self.log('Clikcked point: [%f, %f]', msg.x, msg.y)
+            lambda msg: rospy.loginfo('Clikcked point: [%f, %f]', msg.x, msg.y)
         )
 
-        self.loginfo("init done")
+        rospy.loginfo("init done")
 
     def run(self):
         while self.keep_alive():
@@ -60,7 +60,7 @@ class state_publisher:
         return not rospy.is_shutdown()
 
     def spin(self):
-        self.log('[%f, %f, %f]', self.state.x, self.state.y, self.state.yaw)
+        rospy.loginfo('[%f, %f, %f]', self.state.x, self.state.y, self.state.yaw)
         self.rviz.log_state(self.state)
         self.rviz.visualize_data()
 
