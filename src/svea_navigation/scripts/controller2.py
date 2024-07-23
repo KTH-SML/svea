7aeb17aa4eaf030f4d0a54bbe7a279ec849fcf3f
ML1907 <marco2.lodetti@mail.polimi.it>
2024-07-23 14:30:38 +0200

    fix gps transform. added changes to local controller to set min velocity

--- src/svea_navigation/scripts/controller2.py
+++ src/svea_navigation/scripts/controller2.py
@@ -7,6 +7,9 @@ from geometry_msgs.msg import Twist
 
 class Controller2(object):
 
+    MIN_SPEED = 0.2
+    DEADZONE = 0 # 0.07
+
     def __init__(self, vehicle_name=''):
         rospy.Subscriber("/cmd_vel", Twist, self.blp_control, queue_size=1)
         self.vehicle_name = vehicle_name
@@ -14,13 +17,19 @@ class Controller2(object):
         self.velocity = 0.0
 
     def blp_control(self, data):
-        self.steering = data.angular.z*1.3
-        self.velocity = data.linear.x
+        raw_steering = data.angular.z 
+        raw_velocity = data.linear.x
 
-        # if self.velocity > 0 and self.velocity < 0.3:
-        #     self.velocity = 0.3
-        # elif self.velocity < 0 and self.velocity > -0.3:
-        #     self.velocity = -0.3
+        if abs(raw_velocity) < self.DEADZONE:
+            self.velocity = 0.0
+        elif raw_velocity >= self.DEADZONE and raw_velocity < self.MIN_SPEED:
+            self.velocity = self.MIN_SPEED
+        elif raw_velocity <= -self.DEADZONE and raw_velocity > -self.MIN_SPEED:
+            self.velocity = -self.MIN_SPEED
+        else:
+            self.velocity = raw_velocity
+
+        self.steering = raw_steering * 1.5
 
 
     def compute_control(self, state):
