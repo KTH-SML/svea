026f31f652dd12910964da74e32fef2634855625
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2024-11-13 21:32:55 +0100

    fix: remove build, devel and reset .gitignore

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/marco/marco/marco_svea/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = ''.split(';') if '' != '' else []
