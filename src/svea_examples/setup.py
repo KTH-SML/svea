914c44efda44d4387a7dfd754536288622bbee84
Li <Li>
2025-05-12 17:28:38 +0200

    update on 05/12/2025

---
+++ src/svea_examples/setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'svea_examples'
+
+def generate_console_scripts(script_dir):
+    console_scripts = []
+    for path in glob(os.path.join(script_dir, '*.py')):
+        filename = os.path.basename(path)
+        module_name = filename[:-3]
+        if module_name == '__init__':
+            continue
+        entry = f'{module_name} = scripts.{module_name}:main'
+        console_scripts.append(entry)
+    return console_scripts
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', [f'resource/{package_name}']),
+        (f'share/{package_name}', ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (f'share/{package_name}/launch', glob('launch/*.xml')),
+        (f'lib/{package_name}', glob('scripts/*.py')),  # Install raw executables to lib/{package_name}
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rosuser',
+    maintainer_email='lic2@kth.se',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': generate_console_scripts('scripts'),
+        # 'console_scripts': [
+        #     'svea_example = script.svea_example:main',
+        #     'svea_example2 = script.svea_example2:main',
+        # ],
+    },
+)
