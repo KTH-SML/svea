f95f26641ae0485d52d715a724b23e0f893d7d51
Li <Li>
2025-07-18 17:19:35 +0200

    actuation interface to low-level reverse fixed

--- src/svea_core/svea_core/models/bicycle.py
+++ src/svea_core/svea_core/models/bicycle.py
@@ -87,7 +87,7 @@ class Bicycle4DWithESC(Bicycle4D):
         """
 
         # With ESC dynamics
-        delta = steering
+        delta = steering * -1
         accel = 1/self.TAU * (velocity - self.vel)
 
         return super().update(delta, accel, **kwds)
