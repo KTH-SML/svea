1ea11f8b12394ff193573860a02b6912332cc1a3
Kaj Munhoz Arfvidsson <kajarfvidsson@gmail.com>
2025-09-30 11:49:00 +0200

    Update package.xml and setup.py files for svea_core, svea_examples, and svea_localization to improve descriptions and maintainers' information

--- src/svea_examples/setup.py
+++ src/svea_examples/setup.py
@@ -1,25 +1,27 @@
-from setuptools import find_packages, setup
 import os
 from glob import glob
+from xml.etree.ElementTree import ET
+from setuptools import find_packages, setup
 
-package_name = 'svea_examples'
+package = ET.parse('package.xml').getroot()
+name = package.find('name').text
 
 setup(
-    name=package_name,
+    name=name,
     version='0.0.0',
-    packages=find_packages(include=[package_name, f"{package_name}.*"]),
+    packages=find_packages(include=[name, f"{name}.*"]),
     data_files=[
-        ('share/ament_index/resource_index/packages', [f'resource/{package_name}']),
-        (f'share/{package_name}', ['package.xml']),
-        (f'share/{package_name}/launch', glob('launch/*.xml')),
-        (f'lib/{package_name}', glob('scripts/*.py')),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        ('share/ament_index/resource_index/packages', [f'resource/{name}']),
+        (f'share/{name}', ['package.xml']),
+        (f'share/{name}/launch', glob('launch/*.xml')),
+        (f'lib/{name}', glob('scripts/*.py')),
+        (os.path.join('share', name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='rosuser',
-    maintainer_email='lic2@kth.se',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer=package.find('maintainer').text,
+    maintainer_email=package.find('maintainer').get('email'),
+    description=package.find('description').text,
+    license=package.find('license').text,
     tests_require=['pytest'],
 )
--- src/svea_localization/setup.py
+++ src/svea_localization/setup.py
@@ -1,27 +1,29 @@
-from setuptools import find_packages, setup
 import os
 from glob import glob
+from setuptools import find_packages, setup
+import xml.etree.ElementTree as ET
 
-package_name = 'svea_localization'
+package = ET.parse('package.xml').getroot()
+name = package.find('name').text
 
 setup(
-    name=package_name,
+    name=name,
     version='0.0.0',
-    packages=find_packages(include=[package_name, f"{package_name}.*"]),
+    packages=find_packages(include=[name, f"{name}.*"]),
     data_files=[
-        ('share/ament_index/resource_index/packages', [f'resource/{package_name}']),
-        (f'share/{package_name}', ['package.xml']),
-        (f'share/{package_name}/launch', glob('launch/*.xml') + glob('launch/**/*.xml')),
-        (f'share/{package_name}/launch', glob('launch/*.py')),
-        (f'share/{package_name}/params', glob('params/*.yaml') + glob('params/**/*.yaml')),
-        (f'lib/{package_name}', glob('scripts/*.py')),
+        ('share/ament_index/resource_index/packages', [f'resource/{name}']),
+        (f'share/{name}', ['package.xml']),
+        (f'share/{name}/launch', glob('launch/*.xml') + glob('launch/**/*.xml')),
+        (f'share/{name}/launch', glob('launch/*.py')),
+        (f'share/{name}/params', glob('params/*.yaml') + glob('params/**/*.yaml')),
+        (f'lib/{name}', glob('scripts/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='rosuser',
-    maintainer_email='lic2@kth.se',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer=package.find('maintainer').text,
+    maintainer_email=package.find('maintainer').get('email'),
+    description=package.find('description').text,
+    license=package.find('license').text,
     tests_require=['pytest'],
 )
 
